ca438578d2ae4d80b87b3c89da4e65b6c4a0ec8b
Jed556 <jguiriba11@gmail.com>
2026-04-01 09:05:13 +0800

    Aligned title bar buttons properly

--- AutoMidiPlayer.WPF/Views/MainWindowView.cs
+++ AutoMidiPlayer.WPF/Views/MainWindowView.cs
@@ -11,12 +11,17 @@ namespace AutoMidiPlayer.WPF.Views;
 public partial class MainWindowView : FluentWindow
 {
     private GlobalHotkeyService? _hotkeyService;
+    private bool _isFullscreen;
+    private WindowState _windowStateBeforeFullscreen = WindowState.Normal;
 
     public MainWindowView()
     {
         InitializeComponent();
         Closing += OnClosing;
         Loaded += OnLoaded;
+        StateChanged += OnStateChanged;
+
+        UpdateWindowButtonState();
     }
 
     private void OnLoaded(object sender, RoutedEventArgs e)
@@ -57,6 +62,72 @@ public partial class MainWindowView : FluentWindow
         // Dispose the hotkey service and tray icon when closing
         _hotkeyService?.Dispose();
         TrayIcon?.Dispose();
+    }
+
+    private void OnStateChanged(object? sender, EventArgs e)
+    {
+        UpdateWindowButtonState();
+    }
+
+    private void UpdateWindowButtonState()
+    {
+        if (MaximizeRestoreGlyph is null || MaximizeRestoreButton is null)
+            return;
+
+        var isMaximized = WindowState == WindowState.Maximized;
+        MaximizeRestoreGlyph.Text = isMaximized ? "\uE923" : "\uE922";
+        MaximizeRestoreButton.ToolTip = isMaximized ? "Restore down" : "Maximize";
+
+        // Fullscreen button isn't necessary but keep this feature
+        // if (FullscreenGlyph is not null && FullscreenButton is not null)
+        // {
+        //     FullscreenGlyph.Text = _isFullscreen ? "\uE73F" : "\uE740";
+        //     FullscreenButton.ToolTip = _isFullscreen ? "Exit full screen" : "Enter full screen";
+        // }
+    }
+
+    private void MinimizeWindow_Click(object sender, RoutedEventArgs e)
+    {
+        WindowState = WindowState.Minimized;
+    }
+
+    private void ToggleMaximizeWindow_Click(object sender, RoutedEventArgs e)
+    {
+        WindowState = WindowState == WindowState.Maximized
+            ? WindowState.Normal
+            : WindowState.Maximized;
+    }
+
+    private void CloseWindow_Click(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+
+    private void ToggleFullscreen_Click(object sender, RoutedEventArgs e)
+    {
+        if (_isFullscreen)
+        {
+            WindowStyle = WindowStyle.SingleBorderWindow;
+            ResizeMode = ResizeMode.CanResize;
+            WindowState = _windowStateBeforeFullscreen == WindowState.Minimized
+                ? WindowState.Normal
+                : _windowStateBeforeFullscreen;
+            _isFullscreen = false;
+        }
+        else
+        {
+            _windowStateBeforeFullscreen = WindowState == WindowState.Minimized
+                ? WindowState.Normal
+                : WindowState;
+
+            WindowState = WindowState.Normal;
+            WindowStyle = WindowStyle.None;
+            ResizeMode = ResizeMode.NoResize;
+            WindowState = WindowState.Maximized;
+            _isFullscreen = true;
+        }
+
+        UpdateWindowButtonState();
     }
 
     private void TrayPlayPause_Click(object sender, RoutedEventArgs e)
