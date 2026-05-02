9a267aae3b6f69c9193850eed5c07d9c8cad67e9
ChaiyoKung <kananek45@gmail.com>
2026-05-02 14:10:18 +0700

    fix(settings): set ActiveNTE default value to true
    Changed the default value of the ActiveNTE setting from false to true to ensure the feature is enabled by default for users.

--- AutoMidiPlayer.Data/Properties/Settings.Designer.cs
+++ AutoMidiPlayer.Data/Properties/Settings.Designer.cs
@@ -548,7 +548,7 @@ namespace AutoMidiPlayer.Data.Properties
 
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool ActiveNTE
         {
             get
